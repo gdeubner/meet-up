dc870a6dbfff11ab6b22afc7867fbab1025737fe
gdeubner <gdeubner@gmail.com>
2022-02-14 01:34:58 -0500

    second commit: added favorites functionality

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -3,14 +3,36 @@ import { createContext, useState } from "react";
 const FavoritesContext = createContext({
     favorites: [],
     totalFavorites: 0,
+    addFavorite: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetup) => {},
 });
 
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
     const [userFavorites, setUserFavorites] = useState([]);
+
+    function addFavoriteHandler(favoriteMeetup) {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.concat(favoriteMeetup);
+        });
+    }
+
+    function removeFavortieHandler(meetupId) {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+        });
+    }
+
+    function itemIsFavoriteHandler(meetupId) {
+        return userFavorites.some((meetup) => meetup.id === meetupId);
+    }
 
     const context = {
         favorites: userFavorites,
         totalFavorites: userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavortieHandler,
+        itemIsFavorite: itemIsFavoriteHandler,
     };
 
     return (
@@ -19,3 +41,5 @@ function FavoritesContextProvider(props) {
         </FavoritesContext.Provider>
     );
 }
+
+export default FavoritesContext;
